a7cf0bcdb3036c2b72fb100decda77da403fa78c
Dalia Galal <dollagalal@gmail.com>
2023-03-30 16:23:50 +0200

    database

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import sqlite3
 
 db = sqlite3.connect("tutorial.db")
-
+cur = db.cursor()
+db.execute("create table habits (name , yes , no)")
 name = input("what is your name?")
 print("Hello",name)
